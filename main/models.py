83192a8712f1a2c288ef2924c5d6ce64d25d0635
hjnata_uchiha <ogo@aga.ru>
2022-02-25 11:45:22 +0300

    add post address

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    short_name = models.CharField(max_length=255)
+    orgn = models.CharField(max_length=13)
+    inn = models.CharField(max_length=12)
+    kpp = models.CharField(max_length=9)
+    post_address = models.CharField(max_length=255)
