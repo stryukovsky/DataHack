f9f3f7fbc3cb724cf88ebbb17ba1a277a156739e
Dmitry Strukovsky <strukovsky1@gmail.com>
2022-02-25 11:52:23 +0300

    chore: indexer is ready

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class University(models.Model):
     short_name = models.CharField(max_length=255)
-    orgn = models.CharField(max_length=13)
+    ogrn = models.CharField(max_length=13)
     inn = models.CharField(max_length=12)
     kpp = models.CharField(max_length=9)
     post_address = models.CharField(max_length=255)
--- workers/universities_parser.py
+++ workers/universities_parser.py
@@ -1,23 +1,42 @@
+import os
 import xml.etree.ElementTree as ET
 
+import django
 
-def analyze_university(university: ET) -> None:
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+
+
+def analyze_university(university_model, university: ET) -> None:
     if university.tag != "Certificate":
         return
+    name = ""
+    ogrn = ""
+    inn = ""
+    kpp = ""
+    address = ""
     for parameter in university:
         if parameter.tag == "RegionName" and parameter.text != "г. Москва":
             continue
         if parameter.tag == "TypeCode" and parameter.text != "Permanent":
             continue
-        name = ""
-        ogrn = ""
-        name = ""
+        if parameter.tag == "EduOrgShortName":
+            name = parameter.text
+        if parameter.tag == "EduOrgINN":
+            inn = parameter.text
+        if parameter.tag == "EduOrgKPP":
+            kpp = parameter.text
+        if parameter.tag == "EduOrgOGRN":
+            ogrn = parameter.text
+        if parameter.tag == "PostAddress":
+            address = parameter.text
+    university_model.objects.create(short_name=name, ogrn=ogrn, inn=inn, kpp=kpp, post_address=address)
 
 
 def main() -> None:
+    django.setup()
     tree = ET.parse('../data/universities.xml')
     root = tree.getroot()
-
+    from main.models import University
     """
     <OpenData><Certificates><Certificate>
     Root is OpenData, but universities are stored in <Certificate> tag
@@ -27,7 +46,7 @@ def main() -> None:
         if child.tag == "Certificates":
             universities = child
             for university in universities:
-                analyze_university(university)
+                analyze_university(University, university)
 
 
 if __name__ == "__main__":
