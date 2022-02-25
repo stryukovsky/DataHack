eaa6e0f30ca6fe39b302b7df03fcd2b5eedb49fd
hjnata_uchiha <ogo@aga.ru>
2022-02-25 11:40:26 +0300

    модель университета

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    short_name = models.CharField(max_length=255)
+    orgn = models.CharField(max_length=13)
+    inn = models.CharField(max_length=12)
+    kpp = models.CharField(max_length=9)
