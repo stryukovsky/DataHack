afa342490d42cb168de7a2989432485d1a257ae0
Dmitry Strukovsky <strukovsky1@gmail.com>
2022-02-25 10:36:22 +0300

    chore: update security

--- DataHack/settings.py
+++ DataHack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*x7u1rz+kh4h#^*h812x7q!@^ri3@e26y^$gw_h@gfu!(ev$)t'
+SECRET_KEY = 'dummy_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
