e3dd22345919ac14c2e4ceda5673d0b89767bebc
Dmitry Strukovsky <strukovsky1@gmail.com>
2022-02-25 16:50:19 +0300

    fix: update views

--- main/views.py
+++ main/views.py
@@ -9,6 +9,10 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "Index.html")
 
 
+def pie_chart(request: HttpRequest) -> HttpResponse:
+    return render(request, "PieChart.html")
+
+
 class UniversityViewSet(ModelViewSet):
     queryset = University.objects.exclude(longitude__isnull=True).exclude(sum__lte=1).order_by('-sum')
     serializer_class = UniversitySerializer
